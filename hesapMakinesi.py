b2c4f1bd72c23a800062b523341e354c38f2aaa4
Burak Altundas <burakaltundas52@gmail.com>
2025-01-30 18:01:06 +0300

    hesap makinesi güncellendi

--- hesapMakinesi.py
+++ hesapMakinesi.py
@@ -1,4 +1,7 @@
+
 def hesapMakinesi(a,b,islem):
+    if islem not in "+-/*":
+        return "yanlis islem secildi"
     if islem=="+":
         return a+b
     if islem =="-":
@@ -8,8 +11,12 @@ def hesapMakinesi(a,b,islem):
     if islem == "*":
         return a*b
 
-a=int(input("Birinci Sayi:"))
-b=int(input("Ikinci Sayi:"))
-islem=input("Islemi Sec:")
-
-print(hesapMakinesi(a,b,islem))
+#main
+while True:
+    try:
+        a=int(input("Birinci Sayi:"))
+        b=int(input("Ikinci Sayi:"))
+        islem=input("Islemi Sec:")
+        print(hesapMakinesi(a,b,islem))
+    except:
+        print("Bir Yanlis Yapildi")
