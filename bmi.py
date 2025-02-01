66ade2f1c72e7e3153b95e231b77db15f9ca98bd
Burak Altundas <burakaltundas52@gmail.com>
2025-02-01 12:46:17 +0300

    Bmi Güncellendi

--- bmi.py
+++ bmi.py
@@ -1,5 +1,10 @@
 def bmi(kg,m):
     return kg/ m**2
+def normal(m):
+    enDusuk=18.5*(m**2)
+    enYuksek= 25*(m**2)
+    print ("En Dusuk Kilonuz:" ,enDusuk ," Olmali")
+    print ("En Yuksek Kilonuz:" ,enYuksek ," Olmali")
 
 kg=int(input("Kg Cinsinden Vucut Agirligi:"))
 m=float(input("Metre Cinsinden Vucut Uzunlugu:"))
@@ -15,3 +20,5 @@ elif bmi<35 and bmi > 30:
     print("Obez")
 else:
     print("Asiri Obez")
+
+normal(m)
